9bb57fc8b4a55ac8dcdb2af450496d354330ebae
Olga Malanova <olgamalanova@gmail.com>
2020-08-16 14:03:03 +0300

    added Image component

---
+++ src/components/Image/Image.js
@@ -0,0 +1,29 @@
+/**
+ * Image component to render image to be edited
+ * @property src path to an image
+ * @property reference - reference will be set to an image element to deal with it
+ */
+import React from 'react';
+import './Image.css';
+
+export class Image extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.props = props
+    }
+    render () {
+        return <div>
+            <img
+                alt="to edit"
+                ref={this.props.reference}
+                src={this.props.src}
+                className="Image"
+                width={255}
+                height={255}
+            />
+        </div>
+    }
+
+}
+export default Image;
